0ee316ea96183e4b1ebbeb6b47a52eb6171b4b69
Alisher Begmatov <alisherbegmatov@yandex.com>
2020-04-07 19:33:34 -0700

    Initial Commit

---
+++ GiphySearchIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearchIOS
+//
+//  Created by Alisher Begmatov on 4/7/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
